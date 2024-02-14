a7435189bbe796a686adf42fa6bb6c5924904b87
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2024-02-13 17:02:43 -0800

    Keep working

--- lamp/app/layout.tsx
+++ lamp/app/layout.tsx
@@ -1,16 +1,18 @@
 import React from "react";
+import { useState } from "react";
 import About from "./about/page";
 import Products from "./products/page";
+import FAQPage from "./faq/page";
 
 const HomePage = () => {
   return (
     <html>
       <body>
         <About />
-        <Faq />
-        <How />
+        {/* <FAQPage />
+        <How /> */}
         <Products />
-        <Contacts />
+        {/* <Contacts /> */}
       </body>
     </html>
   );
